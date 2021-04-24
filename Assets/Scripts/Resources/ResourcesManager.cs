478fb8cee29c6e4324c4409b548e927125e3d8f0
Xwilarg <xwilarg@yahoo.fr>
2021-04-24 21:47:04 +0200

    Add resources consumption

--- Assets/Scripts/Resources/ResourcesManager.cs
+++ Assets/Scripts/Resources/ResourcesManager.cs
@@ -7,6 +7,13 @@ namespace Scripts.Resources
 {
     public class ResourcesManager : MonoBehaviour
     {
+        public static ResourcesManager S;
+
+        private void Awake()
+        {
+            S = this;
+        }
+
         [SerializeField]
         private Text _resourcesText;
 
@@ -21,6 +28,16 @@ namespace Scripts.Resources
         private void UpdateUI()
         {
             _resourcesText.text = "Resources:\n" + string.Join("\n", _resources.Select(x => x.Key + ": " + x.Value));
+        }
+
+        public bool ConsumeResource(ResourceType type, int amount)
+        {
+            if (!_resources.ContainsKey(type) || _resources[type] < amount)
+            {
+                return false;
+            }
+            _resources[type] -= amount;
+            return true;
         }
     }
 }
