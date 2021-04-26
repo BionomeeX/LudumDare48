eb836abfd58b82ce378f1675eb6ce8545b27d11d
Clement Carre <clement.carre@bionomeex.com>
2021-04-26 15:37:59 +0200

    Does not fix bug

---
+++ Assets/Scripts/Map/Room/LiftRoom.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Scripts.Map.Room.ModulableRoom
+{
+    public class LiftRoom : ARoom
+    {
+        public LiftRoom(Vector2Int size, Vector2Int position) : base(size, position)
+        { }
+
+        public override float GetCost()
+            => .1f;
+
+        protected override string GetNameInternal()
+            => "Lift";
+
+        protected override string GetDescriptionInternal()
+            => "Can be used to go up and down ... Weeeeeeeeeeeeeeeeeeeeeeeeeee!";
+    }
+}
