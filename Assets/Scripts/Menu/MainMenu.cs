99b80c2c007757f4c8d9a1ea7ed04ba0541e50d4
Xwilarg <xwilarg@yahoo.fr>
2021-04-27 00:17:02 +0200

    Add sprite gallery

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scripts.Menu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void Play()
+        {
+            SceneManager.LoadScene("Main");
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+
+        private void Start()
+        {
+            foreach (var i in Images)
+                i.SetActive(false);
+        }
+
+        public GameObject[] Images;
+        public void LoadImage(int index)
+        {
+            foreach (var i in Images)
+                i.SetActive(false);
+            Images[index].SetActive(true);
+        }
+    }
+}
